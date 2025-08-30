77ec648a358a9551408c96c09539adb610ffd830
bieldnz <gabriel.dinizdesouza@hotmail.com>
2025-08-30 10:52:04 -0300

    first commit

---
+++ src/main/java/model/Ator.java
@@ -0,0 +1,38 @@
+package model;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Ator implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private long id;
+	
+	private String nome;
+	private String pais;
+	
+	public Ator() {
+		
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getPais() {
+		return pais;
+	}
+	public void setPais(String pais) {
+		this.pais = pais;
+	}
+}
